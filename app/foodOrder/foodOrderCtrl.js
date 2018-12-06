d20338117c02981b11073b093b6f4926d89acd07
smadip <smadip@gmail.com>
2018-12-06 15:05:09 +0200

    add button to foodOption page

--- app/foodOrder/foodOrderCtrl.js
+++ app/foodOrder/foodOrderCtrl.js
@@ -6,7 +6,20 @@ app.controller("foodOrderCtrl", function ($scope, foodSrv) {
         $scope.foods = options;
     }, function (error) {
 
-    })    
+    })   
+    
+    $scope.setOrderDetails = function(){
+        for(var i=0;i<$scope.foods.length;i++){
+
+        }
+    }
+
+    $scope.onSelected = function(){
+        if($scope.isSelected)
+        {
+            
+        }
+    }
 
     // $scope.setSelectedFoodType = function (foodType) {
     //     $scope.selectedFoodType = foodType;
