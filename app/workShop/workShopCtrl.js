d1fb8d0ca7cdabe2f813c9b3fd7a315006984431
smadip <smadip@gmail.com>
2018-12-10 15:10:18 +0200

    todo remark

--- app/workShop/workShopCtrl.js
+++ app/workShop/workShopCtrl.js
@@ -11,4 +11,18 @@ app.controller("workShopCtrl", function ($scope, workShopSrv) {
     }, function(error) {
         
     })
+
+    $scope.RegisterToWorkShop = function(){
+        
+        //ToDo: set all parameters of work shope and send email
+        //add button save local PDF in order food
+        //Add button back in order and workshop
+        //add login - 
+        //after login  show admin activation or user activation squer
+        //admin add new workShop - see that the user can choose this work shop that just added
+        //add for for order design cake
+
+        $scope.selectedWorkShop
+        workShopSrv.Register();
+    }
 });
