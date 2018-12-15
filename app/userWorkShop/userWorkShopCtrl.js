d01f937c4fa5405af544fbf9c33552cb074d3d07
smadip <smadip@gmail.com>
2018-12-15 23:26:52 +0200

    carusel

--- app/userWorkShop/userWorkShopCtrl.js
+++ app/userWorkShop/userWorkShopCtrl.js
@@ -14,12 +14,18 @@ app.controller("userWorkShopCtrl", function ($scope, userSrv, workShopSrv) {
 
                 workShops.forEach(ws => {
                     if (workShopList.includes(ws.id)) {
-                        ws.imgUrl = ws.images[1];
+                        ws.imgUrl = ws.images[0];
                         ws.pageLink = "#!/userWorkShopDetails"
+                        ws.active = false;
                         $scope.cards.push(ws);
                     }
 
                 });
+
+                for (var i=0; i<4; i++) {
+                    $scope.addSlide(i);
+                  }
+                  
             }, function (error) {
 
             })
@@ -32,4 +38,44 @@ app.controller("userWorkShopCtrl", function ($scope, userSrv, workShopSrv) {
 
     $scope.getUserWorkShop();
 
-});
+    var slides = $scope.slides = [];
+    $scope.addSlide = function(i) {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+        image: $scope.selectedCard.images[i].imgUrl + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittens', 'Felines', 'Cuties'][slides.length % 4]
+      });
+    };
+    
+    
+    $scope.getActiveSlide = function () {
+      var activeSlide = slides.filter(function (s) { 
+        return s.active;
+      })[0];
+      
+      alert(slides.indexOf(activeSlide));
+      
+    };
+  
+    $scope.setActiveSlide = function (idx) {
+      $scope.slides[idx].active=true;
+    };
+    
+  });
+  
+  
+  app.directive('carouselControls', function() {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        scope.goNext = function() {
+          element.isolateScope().next();
+        };
+        scope.goPrev = function() {
+          element.isolateScope().prev();
+        };
+        
+      }
+    };
+  });
