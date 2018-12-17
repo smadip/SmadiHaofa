4eca58edb47ef85562a80f69c7dd7d6f13f1c38e
smadip <smadip@gmail.com>
2018-12-17 09:25:05 +0200

    add back button

--- app/userWorkShop/userWorkShopCtrl.js
+++ app/userWorkShop/userWorkShopCtrl.js
@@ -1,102 +1,71 @@
 app.controller("userWorkShopCtrl", function ($scope, userSrv, workShopSrv) {
 
   $scope.activeIndex = 0;
-  
-    $scope.selectedCard = workShopSrv.getSelectedUserCard();
-    $scope.cards = [];
-    $scope.getUserWorkShop = function () {
 
-        $scope.activeUser = userSrv.getActiveUser();
-        if ($scope.activeUser != null) {
+  $scope.selectedCard = workShopSrv.getSelectedUserCard();
+  $scope.cards = [];
+  $scope.getUserWorkShop = function () {
 
-            var workShopList = $scope.activeUser.workShopList;
+    $scope.activeUser = userSrv.getActiveUser();
+    if ($scope.activeUser != null) {
 
-            var workShops = workShopSrv.getWorkShopOptions(false).then(function (options) {
-                workShops = options;
+      var workShopList = $scope.activeUser.workShopList;
 
-                workShops.forEach(ws => {
-                    if (workShopList.includes(ws.id)) {
-                        ws.imgUrl = ws.images[0];
-                        ws.pageLink = "#!/userWorkShopDetails"
-                        ws.active = false;
-                        $scope.cards.push(ws);
-                    }
+      var workShops = workShopSrv.getWorkShopOptions(false).then(function (options) {
+        workShops = options;
 
-                });
+        workShops.forEach(ws => {
+          if (workShopList.includes(ws.id)) {
+            ws.imgUrl = ws.images[0];
+            ws.pageLink = "#!/userWorkShopDetails"
+            ws.active = false;
+            $scope.cards.push(ws);
+          }
 
-                // for (var i=0; i<4; i++) {
-                //     $scope.addSlide(i);
-                //   }
-                  
-            }, function (error) {
+        });
 
-            })
-        }
-    }
+        // for (var i=0; i<4; i++) {
+        //     $scope.addSlide(i);
+        //   }
 
-    $scope.cardSelected = function (card) {
-        workShopSrv.setSelectedUserCard(card);
-    }
+      }, function (error) {
 
-    $scope.getUserWorkShop();
-
-    $scope.nextImg = function(activeIndex, propImages) {
-      if (activeIndex === propImages.length -1) {
-        $scope.activeIndex = 0;
-      }
-      else{
-        $scope.activeIndex +=1;
-      }
-    
-  };
-
-  $scope.prevImg = function(activeIndex, propImages) {
-    if (activeIndex === 0) {
-      $scope.activeIndex = propImages.length -1;
-    }
-    else{
-      $scope.activeIndex -=1;
+      })
     }
   }
 
-    // var slides = $scope.slides = [];
-    // $scope.addSlide = function(i) {
-    //   var newWidth = 600 + slides.length + 1;
-    //   slides.push({
-    //     image: $scope.selectedCard.images[i].imgUrl,
-    //     text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-    //       ['Cats', 'Kittens', 'Felines', 'Cuties'][slides.length % 4]
-    //   });
-    // };
-    
-    
-    // $scope.getActiveSlide = function () {
-    //   var activeSlide = slides.filter(function (s) { 
-    //     return s.active;
-    //   })[0];
-      
-    //   alert(slides.indexOf(activeSlide));
-      
-    // };
+  $scope.cardSelected = function (card) {
+    workShopSrv.setSelectedUserCard(card);
+  }
+
+  $scope.getUserWorkShop();
+
+  $scope.nextImg = function (activeIndex, propImages) {
+    if (activeIndex === propImages.length - 1) {
+      $scope.activeIndex = 0;
+    }
+    else {
+      $scope.activeIndex += 1;
+    }
+
+  };
+
+  $scope.prevImg = function (activeIndex, propImages) {
+    if (activeIndex === 0) {
+      $scope.activeIndex = propImages.length - 1;
+    }
+    else {
+      $scope.activeIndex -= 1;
+    }
+  }
+
+  $scope.backToHomePage = function () {
+    window.location = "#!/";
+  }
+
+  $scope.backToUserWorkShop = function () {
+    window.location = "#!/userWorkShop";
+  }
   
-    // $scope.setActiveSlide = function (idx) {
-    //   $scope.slides[idx].active=true;
-    // };
-    
-  });
-  
-  
-  // app.directive('carouselControls', function() {
-  //   return {
-  //     restrict: 'A',
-  //     link: function (scope, element, attrs) {
-  //       scope.goNext = function() {
-  //         element.isolateScope().next();
-  //       };
-  //       scope.goPrev = function() {
-  //         element.isolateScope().prev();
-  //       };
-        
-  //     }
-  //   };
-  // });
+});
+ 
